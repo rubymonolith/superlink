ec15f4d43841c9e2933168fb13499d67c5f20502
Brad Gessler <bradgessler@gmail.com>
2025-03-14 15:14:17 -0700

    Initial commit

---
+++ spec/superlink_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Superlink do
+  it "has a version number" do
+    expect(Superlink::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
